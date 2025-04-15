d9e9aba52f8d5b6b68d4a99eadd3a39278361ec0
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2025-04-15 17:42:37 +0200

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+const API_KEY = "b43ee0f5c95e87749bb31d1f361e9d6b"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+}
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results
+}
